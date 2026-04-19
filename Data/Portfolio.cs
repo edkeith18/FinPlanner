8f295a477a10c0da89e6f8f59c381797c0004e63
edkeith18 <46012014+edkeith18@users.noreply.github.com>
2026-04-18 17:49:14 -0700

    Consolidate profile pages into Financial Profile (#9)

--- Data/Portfolio.cs
+++ Data/Portfolio.cs
@@ -2,20 +2,45 @@
 
 public class Portfolio
 {
-	public List<Account> Accounts { get; set; } = new();
-	public decimal Year { get; set; } = 0;
-	public decimal SecuritiesAnnualInterestRate { get; set; } = 0.0m;
+    public List<Account> Accounts { get; set; } = new();
+    public List<IncomeItem> Income { get; set; } = new();
+    public List<ExpenseItem> Expenses { get; set; } = new();
+    public decimal Year { get; set; } = 0;
+    public decimal SecuritiesAnnualInterestRate { get; set; } = 0.0m;
 
-	public void AddAccount(string name, decimal balance)
-	{
+    public void AddAccount(string name, decimal balance)
+    {
         Accounts.Add(new Account { Name = name, Balance = balance, LastUpdated = DateTime.Now });
     }
 
+    public void AddIncome(string name, decimal amount)
+    {
+        Income.Add(new IncomeItem { Name = name, Amount = amount, LastUpdated = DateTime.Now });
+    }
+
+    public void AddExpense(string name, decimal amount)
+    {
+        Expenses.Add(new ExpenseItem { Name = name, Amount = amount, LastUpdated = DateTime.Now });
+    }
 }
 
 public class Account
 {
-	public string Name { get; set; } = "";
-	public decimal Balance { get; set; }
-	public DateTime LastUpdated { get; set; }
+    public string Name { get; set; } = "";
+    public decimal Balance { get; set; }
+    public DateTime LastUpdated { get; set; }
+}
+
+public class IncomeItem
+{
+    public string Name { get; set; } = "";
+    public decimal Amount { get; set; }
+    public DateTime LastUpdated { get; set; }
+}
+
+public class ExpenseItem
+{
+    public string Name { get; set; } = "";
+    public decimal Amount { get; set; }
+    public DateTime LastUpdated { get; set; }
 }
